a6644358a873fedbfb35b3d0fcc230664658b2df
root <rouskoi@live.com>
2022-06-07 19:40:06 -0700

    setting up of some basic site structure and components

---
+++ src/Components/PhotoframeComponent.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const photoContainer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default photoContainer
+
+
+
+/*     
+- Comnponent will be main box for lists of photos I decide to upload / host. NOTE: I may still make a seperate component that is an expanded photo view. 
+- TODO: I need to recieve what props?
+    - Photo display / frame size.
+    - photo url / location.
+    - padding probably.
+    - Potentially photo title / label / summary NOTE: I think Id like for it to be on mouse hover as well, for photo info.
+    -
+
+
+
+
+
+
+
+*/
